8e90ce320d7fef2735fae57a5847c13b75b47701
Yufenyuy Veyeh Didier <yveyeh@gmail.com>
2021-08-03 13:29:37 +0100

    Enhancements... v1.1

---
+++ php/functions.php
@@ -0,0 +1,41 @@
+<?php
+
+    /** */
+    function getKeys($_keys_str) {
+        $str = sanitizeKeysString($_keys_str, ",");
+        $keys = explode(",", $str);
+        return $keys;
+    }
+
+    /**
+     * Removes `,` from the beginning or end of a given string.
+     * @param {string} $_str - The string to sanitize.
+     * @param {string} $_char - The character to remove.
+     * @return String
+     */
+    function sanitizeKeysString($_str, $_char) {
+        $sanitized_str = "";
+        if (startsWith($_str, $_char)) {
+            $sanitized_str = substr_replace($_str , "", 0);
+            if (endswith($sanitized_str, $_char)) {
+                $sanitized_str = substr_replace($_str , "", -1);
+            }
+        }
+        if (endsWith($_str, $_char)) {
+            $sanitized_str = substr_replace($_str , "", -1);
+            if (startswith($sanitized_str, $_char)) {
+                $sanitized_str = substr_replace($_str , "", 0);
+            }
+        }
+        return $sanitized_str;
+    }
+
+    function startsWith($haystack, $needle) {
+        return ($needle === $haystack[0]);
+    }
+
+    function endsWith($haystack, $needle) {
+        return ($needle === $haystack[strlen($haystack) - 1]);
+    }
+
+?>
--- php/import.php
+++ php/import.php
@@ -1,77 +1,49 @@
 <?php
 
+    include './functions.php';
+
     // string of entered keys
-    $keys_string = $_POST['keys'];
+    $keys_str = $_POST['keys'];
     // the xml file
     $temp_file = $_FILES['file']['tmp_name'];
 
+    //
     $data = array();
-    $results = array();
+    //
+    $values = array();
+    //
+    $keys = getKeys($keys_str);
 
-    function sanitizeKeysString($_string, $_char) {
-        $sanitized_string = "";
-        if (startsWith($_string, $_char)) {
-            $sanitized_string = substr_replace($_string , "", 0);
-            if (endswith($sanitized_string, $_char)) {
-                $sanitized_string = substr_replace($_string , "", -1);
-            }
-        }
-        if (endsWith($_string, $_char)) {
-            $sanitized_string = substr_replace($_string , "", -1);
-            if (startswith($sanitized_string, $_char)) {
-                $sanitized_string = substr_replace($_string , "", 0);
-            }
-        }
-        return $sanitized_string;
-    }
-
-    function startsWith($haystack, $needle) {
-        return ($needle === $haystack[0]);
-    }
-
-    function endsWith($haystack, $needle) {
-        return ($needle === $haystack[strlen($haystack) - 1]);
-    }
-
+    //
     if ( $temp_file ) {
 
         $dom = DOMDocument::load( $temp_file );
         $rows = $dom->getElementsByTagName( 'Row' );
+        //
         $first_row = true;
-        
-        foreach ($rows as $row) {
+        //
+        foreach ( $rows as $row ) {
 
             if ( !$first_row ) {
-
-                
-                $index = 1;
+                //
                 $cells = $row->getElementsByTagName( 'Cell' );
-
+                //
                 foreach( $cells as $cell ) {
-
-                    $ind = $cell->getAttribute( 'Index' );
-                    if ( $ind != null ) $index = $ind;
-                    
-                    if ( $index == 1 ) $code_catalogue = $cell->nodeValue;
-                    if ( $index == 2 ) $items = $cell->nodeValue;
-                    if ( $index == 3 ) $kit_size = $cell->nodeValue;
-                    if ( $index == 4 ) $method = $cell->nodeValue;
-                    
-                    $index += 1;
+                    array_push( $values, $cell->nodeValue );
                 }
-
-                $data_ = array();
-                $data_['code_catalogue'] = $code_catalogue;
-                $data_['items'] = $items;
-                $data_['kit_size'] = $kit_size;
-                $data_['method'] = $method;
-                $results[] = $data_;
+                //
+                for ($i=0; $i < count($keys); $i++) { 
+                    $_data[ $keys[$i] ] = $values[$i];
+                }
+                //
+                $data[] = $_data;
             }
-
+            //
             $first_row = false;
         }
-    }
 
-    echo json_encode($results);
+    }
+    //
+    echo json_encode($data);
 
 ?>
